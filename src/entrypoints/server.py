af3927405e58357b7517a2a3569521d88b7f82df
Victor <victorcoelho250602@gmail.com>
2025-08-15 05:03:51 -0300

    Changed UV to Pip, changed docker image to trixie-slim and unbalanced server instances. Changed JSON parsing to ORJson.

--- src/entrypoints/server.py
+++ src/entrypoints/server.py
@@ -1,13 +1,19 @@
 import json
 
-from fastapi import FastAPI, Response, Query
+from fastapi import FastAPI, Request, Response, Query
+from fastapi.responses import ORJSONResponse
+from starlette.requests import Request as StarletteRequest
+import orjson
 
 import src.broker  # noqa: F401
-from src.models.payment import Payment
 from src.services.process_payment import payment_service
 from src.services.summary import summary_service
 
-app = FastAPI()
+async def orjson_json(self):
+    return orjson.loads(await self.body())
+StarletteRequest.json = orjson_json
+
+app = FastAPI(default_response_class=ORJSONResponse)
 
 
 @app.get("/payments-summary")
@@ -17,6 +23,6 @@ async def get_payments_summary(from_utc: str = Query(alias="from"),
     return Response(json.dumps(summary), status_code=200)
 
 @app.post("/payments")
-async def process_payment(payment: Payment) -> Response:
-    payment_service.send(payment.model_dump())
+async def process_payment(request: Request) -> Response:
+    payment_service.send(await request.json())
     return Response(status_code=200)
--- src/models/payment.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class Payment(BaseModel):
-    correlationId: str
-    amount: float
